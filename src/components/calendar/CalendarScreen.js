9b4f6d5e7dbd648b13a2f20ac94811b90db3db39
Andres Mestra <andresmestra11a@gmail.com>
2021-01-27 19:37:41 -0500

    Estructura NavBar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import { NavBar } from '../ui/NavBar'
 
 export const CalendarScreen = () => {
   return (
     <div>
-      <h1>CalendarApp</h1>
+      <NavBar />
     </div>
   )
 }
---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import './navbar.css'
+
+export const NavBar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark md-4">
+      <span className="navbar-brand">
+        Juan
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i>
+        <span>Salir</span>
+      </button>
+    </div>
+  )
+}
