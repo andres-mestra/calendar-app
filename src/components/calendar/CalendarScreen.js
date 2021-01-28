3b8f540b839dfc89ad834beb63ca48242c2a4b35
Andres Mestra <andresmestra11a@gmail.com>
2021-01-27 19:54:15 -0500

    Estructura inicial calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,28 @@
 import * as React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
 import { NavBar } from '../ui/NavBar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment)
+const events = [{
+  title: 'Cumpleaños del jefe',
+  start: moment().toDate(), //New Date()
+  end: moment().add(2, 'hours').toDate(),
+  bgColor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
       <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   )
 }
