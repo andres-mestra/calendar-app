93cdb0e2a49d4594087a20a7a574c7d9f0d4dcd6
Andres Mestra <andresmestra11a@gmail.com>
2021-01-27 20:34:04 -0500

    Configurando calendario en español

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,17 +3,38 @@ import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
 import { NavBar } from '../ui/NavBar'
+import { messages } from '../helpers/calendar-message'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es'
+
+moment.locale('es')
 
 const localizer = momentLocalizer(moment)
 const events = [{
   title: 'Cumpleaños del jefe',
   start: moment().toDate(), //New Date()
   end: moment().add(2, 'hours').toDate(),
-  bgColor: '#fafafa'
+  bgColor: '#fafafa',
+  notas: ['comprar juego']
 }]
 
 export const CalendarScreen = () => {
+  
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected)
+    const style =  {
+      backgroundColor:'#367CF7',
+      borderRadius:'0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <div className="calendar-screen">
       <NavBar />
@@ -22,6 +43,8 @@ export const CalendarScreen = () => {
         events={ events }
         startAccessor="start"
         endAccessor="end"
+        messages={ messages }
+        eventPropGetter={ eventStyleGetter }
       />
     </div>
   )
