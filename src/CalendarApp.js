8047de2d584ae88bf677f5e045b156c2d5fd24b9
Andres Mestra <andresmestra11a@gmail.com>
2021-01-28 19:24:49 -0500

    Configuración inicial del store de redux

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './components/store/store'
 import { AppRouter } from './components/Router/AppRouter'
 
 export const CalendarApp = () => {
   return (
-    <>
+    <Provider store={ store }>
       <AppRouter />
-    </>
+    </Provider>
   )
 }
